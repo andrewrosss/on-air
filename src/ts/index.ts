d1c081fc6eb6375479a1a122e470cd040e940ee4
Andrew Ross <andrew.ross.mail@gmail.com>
2024-02-10 21:06:56 -0500

    Refactor index.ts
    Convert MediaListener to LogTailer class; Add LightController class; Add support for mic detection

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,31 +1,30 @@
 import type { Subprocess } from "bun";
 
-const MAKER_WEBHOOK_KEY = nullthrows(Bun.env.MAKER_WEBHOOK_KEY);
-const MAKER_ON_AIR_EVENT = nullthrows(Bun.env.MAKER_ON_AIR_EVENT);
-const MAKER_OFF_AIR_EVENT = nullthrows(Bun.env.MAKER_OFF_AIR_EVENT);
-
-type MediaEventType = "camera_on" | "camera_off" | "mic_on" | "mic_off";
-type MediaEventHandler = (
-  state: MediaState,
-  event: MediaEventType
+type LogEventType = "camera_on" | "camera_off" | "mic_on" | "mic_off";
+type LogState = { camera: boolean; mic: boolean };
+type LogSubscriber = (
+  state: LogState,
+  event: LogEventType
 ) => void | Promise<void>;
-type MediaState = { camera: boolean; mic: boolean };
 
-class MediaListener {
-  #callbacks: Array<MediaEventHandler> = [];
+class LogTailer {
+  #subscribers: Array<LogSubscriber> = [];
   #process: Subprocess<"ignore", "pipe", "inherit"> | null = null;
-  #state: MediaState = { camera: false, mic: false };
+  #state: LogState = { camera: false, mic: false };
 
-  addHandler(callback: MediaEventHandler) {
-    this.#callbacks.push(callback);
+  subscribe(subscriber: LogSubscriber) {
+    this.#subscribers.push(subscriber);
+    return () => {
+      this.#subscribers = this.#subscribers.filter((sub) => sub !== subscriber);
+    };
   }
 
-  removeHandler(callback: MediaEventHandler) {
-    this.#callbacks = this.#callbacks.filter((cb) => cb !== callback);
+  unsubscribe(subscriber: LogSubscriber) {
+    this.#subscribers = this.#subscribers.filter((sub) => sub !== subscriber);
   }
 
   async start() {
-    console.log("Starting media listener");
+    log("Starting log tailer");
 
     // TODO: make this work for different mac versions
     // TODO: make this configurable
@@ -33,7 +32,12 @@ class MediaListener {
       "log",
       "stream",
       "--predicate",
-      'sender contains "appleh13camerad" and (composedMessage contains "ConnectClient" or composedMessage contains "DisconnectClient")',
+      [
+        // camera
+        '(process == "appleh13camerad" and (composedMessage contains "ConnectClient" or composedMessage contains "DisconnectClient"))',
+        // mic
+        '(process == "coreaudiod" and subsystem == "com.apple.coreaudio" and (composedMessage contains "Starting {" or composedMessage contains "Stopping {"))',
+      ].join(" or "),
     ]);
 
     try {
@@ -47,13 +51,19 @@ class MediaListener {
         if (line.includes("Filtering the log data using")) continue;
 
         if (line.includes("ConnectClient")) {
+          this.#subscribers.forEach((sub) => sub(this.#state, "camera_on"));
           this.#state.camera = true;
-          this.#callbacks.forEach((cb) => cb(this.#state, "camera_on"));
         } else if (line.includes("DisconnectClient")) {
+          this.#subscribers.forEach((sub) => sub(this.#state, "camera_off"));
           this.#state.camera = false;
-          this.#callbacks.forEach((cb) => cb(this.#state, "camera_off"));
+        } else if (line.includes("Starting {")) {
+          this.#subscribers.forEach((sub) => sub(this.#state, "mic_on"));
+          this.#state.mic = true;
+        } else if (line.includes("Stopping {")) {
+          this.#subscribers.forEach((sub) => sub(this.#state, "mic_off"));
+          this.#state.mic = false;
         } else {
-          console.log("Unknown log line:", line);
+          log("Unknown log line:", line);
         }
       }
     } catch (err) {
@@ -62,26 +72,43 @@ class MediaListener {
   }
 }
 
-async function defaultHandler(state: MediaState, curr: MediaEventType) {
-  console.log(`State = ${JSON.stringify(state)}, Current = ${curr}`);
-  if (curr === "camera_on") await sendOnAir();
-  else if (curr === "mic_on") await sendOnAir();
-  // only send off-air if both camera and mic are off
-  else if (curr === "camera_off" && !state.mic) await sendOffAir();
-  else if (curr === "mic_off" && !state.camera) await sendOffAir();
+interface ILightController {
+  on(): Promise<void>;
+  off(): Promise<void>;
 }
 
-// TODO: debounce the handlers
-async function sendOnAir() {
-  const url = `https://maker.ifttt.com/trigger/${MAKER_ON_AIR_EVENT}/with/key/${MAKER_WEBHOOK_KEY}`;
-  const init = { method: "POST" };
-  return await fetch(url, init);
+class LightController implements ILightController {
+  constructor(private onURL: string, private offURL: string) {}
+
+  static fromEnv() {
+    const MAKER_WEBHOOK_KEY = nullthrows(Bun.env.MAKER_WEBHOOK_KEY);
+    const MAKER_ON_AIR_EVENT = nullthrows(Bun.env.MAKER_ON_AIR_EVENT);
+    const MAKER_OFF_AIR_EVENT = nullthrows(Bun.env.MAKER_OFF_AIR_EVENT);
+
+    const onURL = `https://maker.ifttt.com/trigger/${MAKER_ON_AIR_EVENT}/with/key/${MAKER_WEBHOOK_KEY}`;
+    const offURL = `https://maker.ifttt.com/trigger/${MAKER_OFF_AIR_EVENT}/with/key/${MAKER_WEBHOOK_KEY}`;
+
+    return new LightController(onURL, offURL);
+  }
+
+  async on() {
+    await fetch(this.onURL, { method: "POST" });
+  }
+
+  async off() {
+    await fetch(this.offURL, { method: "POST" });
+  }
 }
 
-async function sendOffAir() {
-  const url = `https://maker.ifttt.com/trigger/${MAKER_OFF_AIR_EVENT}/with/key/${MAKER_WEBHOOK_KEY}`;
-  const init = { method: "POST" };
-  return await fetch(url, init);
+function logSubscriberFactory(light: ILightController): LogSubscriber {
+  return async (state, event) => {
+    log(`State = ${JSON.stringify(state)}, Event = ${event}`);
+    if (event === "camera_on") await light.on();
+    else if (event === "mic_on") await light.on();
+    // only send off-air if both camera and mic are off
+    else if (event === "camera_off" && !state.mic) await light.off();
+    else if (event === "mic_off" && !state.camera) await light.off();
+  };
 }
 
 function nullthrows<T>(value: T | null | undefined, message?: string): T {
@@ -91,12 +118,23 @@ function nullthrows<T>(value: T | null | undefined, message?: string): T {
   throw new Error(message || "Got unexpected null or undefined value");
 }
 
+function log(...data: any[]) {
+  const date_str = new Date().toISOString();
+  console.log(`[${date_str}]`, ...data);
+}
+
 if (import.meta.main) {
-  const listener = new MediaListener();
-  listener.addHandler(defaultHandler);
-  listener.start();
+  const light = LightController.fromEnv();
+  const subscriber = logSubscriberFactory(light);
+  const tailer = new LogTailer();
+  tailer.subscribe(subscriber);
+  tailer.start();
+
+  const port = Bun.env.PORT ?? 4417;
+  log(`Starting server on port ${port}`);
 
   Bun.serve({
+    port,
     fetch(req) {
       return new Response("Bun!");
     },
